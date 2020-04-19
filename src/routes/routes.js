97e44403692ff6f4b703ac4938336154a6a4fd43
Arthur <alves.m.arthur@gmail.com>
2020-04-19 06:19:24 -0300

    Rota para atualizar o perfil do usuario

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
+const axios = require('axios');
+
 const User = require('../models/User');
-
 const sendEmail = require('../services/nodeMailer');
-
 const authRoutes = require('./authRoutes');
-const {profileMiddleware, homeMiddleware} = require('./middlewares');
+const { profileMiddleware, homeMiddleware } = require('./middlewares');
 
 const routes = Router();
 
@@ -19,58 +19,77 @@ routes.get('/', homeMiddleware, (req, res) => {
     res.render('pages/login');
 });
 
-routes.get('/profile', profileMiddleware, (req,res) =>{
-    res.render('pages/profile',{profile: req.user});
+routes.get('/profile', profileMiddleware, (req, res) => {
+    res.render('pages/profile', { profile: req.user });
 });
 
-routes.get('/logout', (req,res) =>{
+routes.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
-routes.get('/tags', async(req,res) =>{
-    let {tags} = await User.findById(req.query.id);
+routes.get('/tags', async (req, res) => {
+    let { tags } = await User.findById(req.query.id);
     res.json(tags);
 })
 
-routes.post('/tags', async(req,res) =>{
-    if(req.body.tag.trim() == ""){
-        res.json({status: 1, res: 'Campo em branco'});
+routes.post('/tags', async (req, res) => {
+    if (req.body.tag.trim() == "") {
+        res.json({ status: 1, res: 'Campo em branco' });
         return;
     }
-    let {tags} = await User.findById(req.body.id);
+    let { tags } = await User.findById(req.body.id);
     var alreadyExists = 0;
 
-    if(tags.length > 0){
+    if (tags.length > 0) {
         tags.forEach(tag => {
-            if(tag == req.body.tag){
+            if (tag == req.body.tag) {
                 alreadyExists = 1;
             }
         });
 
-        if(alreadyExists == 0){
+        if (alreadyExists == 0) {
             tags.push(req.body.tag);
-            await User.updateOne({_id: req.body.id},{tags: tags});
-            res.json({status: 0, res: 'Tag inserida com sucesso'});
+            await User.updateOne({ _id: req.body.id }, { tags: tags });
+            res.json({ status: 0, res: 'Tag inserida com sucesso' });
             return;
         }
-        res.json({status: 1, res: 'Tag já Existe'});
+        res.json({ status: 1, res: 'Tag já Existe' });
         return;
     }
 
     tags.push(req.body.tag);
-    await User.updateOne({_id: req.body.id},{tags: tags});
-    res.json({status: 0, res: 'Tag inserida com sucesso'});
+    await User.updateOne({ _id: req.body.id }, { tags: tags });
+    res.json({ status: 0, res: 'Tag inserida com sucesso' });
 });
 
-routes.delete('/tags', async(req,res) =>{
-     let {tags} = await User.findById(req.body.id);
-     tags = tags.filter(tag => (tag != req.body.tag));
-     await User.updateOne({_id: req.body.id},{tags: tags}).catch(()=>{
-         res.json({status: 1, res: 'Erro ao deletar a Tag'});
-         return;
-     });
-     res.json({status: 0, res: 'Tag deletada com sucesso'});
+routes.delete('/tags', async (req, res) => {
+    let { tags } = await User.findById(req.body.id);
+    tags = tags.filter(tag => (tag != req.body.tag));
+    await User.updateOne({ _id: req.body.id }, { tags: tags }).catch(() => {
+        res.json({ status: 1, res: 'Erro ao deletar a Tag' });
+        return;
+    });
+    res.json({ status: 0, res: 'Tag deletada com sucesso' });
 });
+
+routes.get('/profileUpdate', async (req, res) => {
+    let { data } = await axios.get(`https://api.github.com/users/${req.user.username}`);
+    let { name, login, avatar_url, location, bio, public_repos } = data;
+    console.log(data)
+    await User.updateOne({ _id: req.user._id },
+        {
+            name,
+            username: login,
+            avatar: avatar_url,
+            location,
+            bio,
+            respositories: public_repos
+        }).catch(() => {
+            res.json({ status: 1, res: 'Erro ao atualizar Perfil' });
+        });
+    res.redirect('/profile');
+    res.json({ status: 0, res: 'Perfil atualizado com sucesso' });
+})
 
 module.exports = routes;
